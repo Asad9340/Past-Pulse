9b4e77262382c8e8e0b09e1aecd24dbb1d66e2df
Asad9340 <asadulimran1999@gmail.com>
2024-03-03 03:52:34 +0600

    added latest section dynamically

--- App.js
+++ App.js
@@ -9,7 +9,6 @@ const allPostAPI = async () => {
 const displayAllPost = posts => {
   const allNewsContainer = document.getElementById('allNewsContainer');
   posts.forEach(post => {
-    console.log(post);
     let isActive = '';
     if (post.isActive) {
       isActive = `<i class="fa-solid fa-circle absolute -top-1 -right-1 text-green-700"></i>`;
@@ -67,8 +66,7 @@ const displayAllPost = posts => {
   });
 };
 
-const cardHandler = (title,view_count) => {
-  console.log('btn clicked', title, view_count);
+const cardHandler = (title, view_count) => {
   const cardHandlerContainer = document.getElementById('cardHandlerContainer');
   const div = document.createElement('div');
   div.innerHTML = `
@@ -84,4 +82,55 @@ const cardHandler = (title,view_count) => {
   `;
   cardHandlerContainer.appendChild(div);
 };
+
+const latestPost = async () => {
+  const res = await fetch(
+    'https://openapi.programming-hero.com/api/retro-forum/latest-posts'
+  );
+  const data = await res.json();
+  showLatestPost(data);
+};
+const showLatestPost = data => {
+  const newsContainer = document.getElementById('newsContainer');
+  data.forEach(news => {
+    console.log(news);
+    const div = document.createElement('div');
+    div.innerHTML = `
+                <div
+              class="p-5 border border-[#12132D26] rounded-xl flex flex-col items-center md:items-start"
+            >
+              <div><img class="rounded-xl mb-4" src="${
+                news.cover_image
+              }" alt="" /></div>
+              <div class="flex gap-4 items-center">
+                <i class="fa-regular fa-calendar"></i>
+                <p class="text-[16px] text-[#12132D99]">${
+                  news.author.posted_date
+                    ? news.author.posted_date
+                    : 'No publish date'
+                }</p>
+              </div>
+              <div class="space-y-3 my-4">
+                <h3 class="text-sm font-bold">${news.title}</h3>
+                <p class="text-[16px] text-[#12132D99]">${news.description}</p>
+              </div>
+              <div class="flex gap-3">
+                <div><img class="rounded-full w-10" src="${
+                  news.profile_image
+                }" alt="" /></div>
+                <div>
+                  <h4 class="text-[16px] text-[#12132D]">${
+                    news.author.name
+                  }</h4>
+                  <p class="text-[14px] text-[#12132D99]">${
+                    news.author.designation?news.author.designation:"Unknown"
+                  }</p>
+                </div>
+              </div>
+            </div>
+    `;
+    newsContainer.appendChild(div);
+  });
+};
+latestPost();
 allPostAPI();
